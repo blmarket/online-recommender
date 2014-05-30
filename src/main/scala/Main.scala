fe3accb7a4f2e77fa34090000d7c6889be03648d
Jeong, Heon <blmarket@gmail.com>
2014-05-30 21:48:53 +0900

    Introduce Example
    running Main class with MovieLens data.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,32 @@
 import java.io.{BufferedReader, FileReader}
+import net.blmarket.recommender.OnlineRecommender
+import scala.util.Random
 
 object Main {
   def main(args: Array[String]) {
     val bfr = new BufferedReader(new FileReader("ml-1m/ratings.dat"))
-    val arr = Iterator.continually(bfr.readLine).takeWhile(_ != null).map(_.split("::")).toArray
-    println("Hello World")
+    val arr = Random.shuffle(Iterator.continually(bfr.readLine).takeWhile(_ != null).map(_.split("::").take(3)).take(500000)).toIndexedSeq
+    val train_pos = arr.length * 9 / 10
+
+    val model = new OnlineRecommender()
+
+    for(i <- 1 to 1000000) {
+      val obj = arr(Random.nextInt(train_pos))
+      model.feed(obj(0), obj(1), 1.0, obj(2).toDouble)
+      if((i % 100000) == 0) {
+        println(i)
+      }
+    }
+
+    var sum: Double = 0.0
+    for(i <- train_pos until train_pos + 10) {
+      val obj = arr(i)
+      val score = model.predict(obj(0), obj(1))
+      val diff = Math.abs(score - obj(2).toDouble)
+
+      // println(score + " " + sum)
+      sum += diff
+    }
+    println("Total Diff : " + sum)
   }
 }
